99f1b2ee9ebc725e51d324b65539a2638911edf1
Tizian <tizian_koller@gmx.ch>
2016-12-01 15:22:50 +0100

    Broadcast + Netwrorkhelper

--- Prg2_4Gewinnt/src/com/viergewinnt/network/GameHoster.java
+++ Prg2_4Gewinnt/src/com/viergewinnt/network/GameHoster.java
@@ -1,5 +1,35 @@
 package com.viergewinnt.network;
 
-public class GameHoster {
+import java.io.Serializable;
+
+public class GameHoster implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String gameName;
+	private String hostAddress;
+	
+	public GameHoster(String gameName, String hostAddress) {
+		this.setGameName(gameName);
+		this.setHostAddress(hostAddress);
+	}
+	
+	public String getGameName() {
+		return gameName;
+	}
+	public void setGameName(String gameName) {
+		this.gameName = gameName;
+	}
+
+	public String getHostAddress() {
+		return hostAddress;
+	}
+
+	public void setHostAddress(String hostAddress) {
+		this.hostAddress = hostAddress;
+	}
 
 }
