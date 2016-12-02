c79802c41f8ae5acfad0121bae81a602e8731936
red <red@Notebook01>
2016-12-02 11:19:24 +0100

    Add time counter

--- Prg2_4Gewinnt/src/com/viergewinnt/gui/panel/StonePanel.java
+++ Prg2_4Gewinnt/src/com/viergewinnt/gui/panel/StonePanel.java
@@ -1,13 +1,17 @@
 package com.viergewinnt.gui.panel;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.LayoutManager;
+import java.awt.Rectangle;
 
 import javax.swing.JPanel;
 
 public class StonePanel extends JPanel {
 
-	public StonePanel() {
-		// TODO Auto-generated constructor stub
+	public StonePanel(Color color) {
+		setBackground(color);
 	}
+	
 
 }
