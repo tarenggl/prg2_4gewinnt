92e3dd68725e817c651c74d928323bb3ff543901
Joel <Joel@Joel-Notebook.home>
2016-12-08 18:29:05 +0100

    AddStoneAction

--- Prg2_4Gewinnt/src/com/viergewinnt/gameobjects/Player.java
+++ Prg2_4Gewinnt/src/com/viergewinnt/gameobjects/Player.java
@@ -6,7 +6,16 @@ public class Player {
 	private Color color;
 	
 	public Player(Color color) {
+		this.setColor(color);
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
 		this.color = color;
 	}
+	
 	
 }
--- Prg2_4Gewinnt/src/com/viergewinnt/gui/panel/GameGrid.java
+++ Prg2_4Gewinnt/src/com/viergewinnt/gui/panel/GameGrid.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Area;
@@ -46,6 +48,29 @@ public class GameGrid extends JPanel{
 			
 			@Override
 			public void mouseDragged(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		addComponentListener(new ComponentListener() {
+			
+			@Override
+			public void componentShown(ComponentEvent e) {
+			}
+			
+			@Override
+			public void componentResized(ComponentEvent e) {
+				game.setGridwidth(getWidth());			
+			}
+			
+			@Override
+			public void componentMoved(ComponentEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void componentHidden(ComponentEvent e) {
 				// TODO Auto-generated method stub
 				
 			}
